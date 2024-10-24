488162cceaf6ae8442e189d14f8a126904ce2253
Farah <f.a.anany@student.aast.edu>
2024-10-24 06:56:07 +0300

    Refactor CSS styles for Login and Register pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Add this line at the top of your file
+
+import "../styles/Login.css";
 
 const Login = () => {
-  return (
-    <div>
-      <h2> Register</h2>
-    </div>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Login
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div className='login'>
+      <h3 className='login-header'>Welcome to Yapper!</h3>
+      <h6 className='login-subheader'>Login</h6>
+      <div className='login-data'>
+        <form className='login-dataform'>
+          <input
+            type="email"
+            className='login-dataformemail'
+            value={email}
+            onChange={handleEmailChange}
+            placeholder='Write your email here'
+          />
+          <input
+            type="password"
+            className='login-dataformpass'
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder='Write your password here'
+          />
+          <button className='login-dataformbtn' type="submit">Log in</button>
+          <small style={{ marginTop: '20px', display: 'block' }}>
+  Don't Have an account? <Link to="/register">Register Here!</Link>
+</small>
+
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,11 +1,62 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import '../styles/Register.css';
+import { Link } from 'react-router-dom'; // Add this line at the top of your file
 const Register = () => {
-  return (
-    <div>
-        <h2>Register</h2>
-    </div>
-  )
-}
+  const [fullname, setFullName] = useState('');
+  const [username, setUsername] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default Register
+  return (
+    <div className='register'>
+      <h3 className='register-header'>Welcome to Yapper!</h3>
+      <h6 className='register-subheader'>Register</h6>
+      <div className='register-data'>
+        <form className='register-dataform'>
+          <input
+            type="text"
+            className='register-dataformfullname'
+            value={fullname}
+            onChange={(e) => setFullName(e.target.value)}  // Use e.target.value
+            placeholder='Full Name'
+          />
+          <input
+            type="text"
+            className='register-dataformusername'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}  // Use e.target.value
+            placeholder='Username'
+          />
+          <input
+            type="email"
+            className='register-dataformemail'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}  // Use e.target.value
+            placeholder='Write your email here'
+          />
+          <input
+            type="password"
+            className='register-dataformpass'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}  // Use e.target.value
+            placeholder='Write your password here'
+          />
+          <input
+            type="password"
+            className='register-dataformconfirm'
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}  // Use e.target.value
+            placeholder='Write your password again'
+          />
+          <button className='register-dataformbtn' type="submit">Register</button>
+          <small style={{ marginTop: '20px', display: 'block' }}>
+ Already Have an Account? <Link to="/login">Login Here!</Link>
+</small>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
