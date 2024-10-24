ecc255e569e1ec1bac5b85f5628865cb74ab9e57
Farah <f.a.anany@student.aast.edu>
2024-10-24 07:12:00 +0300

    Refactor package dependencies and add eye icon for password visibility

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Add this line at the top of your file
-
+import { Link } from 'react-router-dom'; // Ensure this import is correct
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // Import FontAwesomeIcon
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'; // Import eye icons
 import "../styles/Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPass, setShowPass] = useState(false); // Fixed state naming convention
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -28,18 +30,22 @@ const Login = () => {
             onChange={handleEmailChange}
             placeholder='Write your email here'
           />
-          <input
-            type="password"
-            className='login-dataformpass'
-            value={password}
-            onChange={handlePasswordChange}
-            placeholder='Write your password here'
-          />
+          <div className="password-container">
+            <input
+              type={showPass ? "text" : "password"} // Use the showPass state correctly
+              className='login-dataformpass'
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder='Write your password here'
+            />
+            <small className='login-showpass' onClick={() => setShowPass(!showPass)}>
+              <FontAwesomeIcon icon={showPass ? faEyeSlash : faEye} />
+            </small>
+          </div>
           <button className='login-dataformbtn' type="submit">Log in</button>
           <small style={{ marginTop: '20px', display: 'block' }}>
-  Don't Have an account? <Link to="/register">Register Here!</Link>
-</small>
-
+            Don't have an account? <Link to="/register">Register Here!</Link>
+          </small>
         </form>
       </div>
     </div>
